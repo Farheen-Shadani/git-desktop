9b3c3922d82ba5370a3db02d5589451c5a024d22
Syeda Taliya <syedatalea97@gmail.com>
2019-09-23 07:54:44 -0700

    signin-button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/SignIn'
 
 function App() {
   return (
+
+
     <div className="App">
+   
+   <SignIn />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/components/SignIn.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../components/SignIn.css'
+
+
+class Header extends Component {
+    render() {
+        return (
+            <>
+
+            <button> Sign In </button>
+
+            <button> Sign Up </button>
+
+            </>
+        );
+    }
+}
+
+export default Header; 
