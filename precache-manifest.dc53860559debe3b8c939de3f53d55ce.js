41c895a97531c73691ab8afce4984e3867798835
LABIT <labit@uit.edu>
2019-09-26 16:14:33 +0500

    Updates

---
+++ precache-manifest.dc53860559debe3b8c939de3f53d55ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d64bc0e694de75d9b098c0d897c6c79",
+    "url": "/git-desktop/index.html"
+  },
+  {
+    "revision": "9cb000894e13cda392a8",
+    "url": "/git-desktop/static/css/main.90d5c4e1.chunk.css"
+  },
+  {
+    "revision": "fa3ad6949cbe95285bb1",
+    "url": "/git-desktop/static/js/2.2dfc36fb.chunk.js"
+  },
+  {
+    "revision": "9cb000894e13cda392a8",
+    "url": "/git-desktop/static/js/main.7d58b257.chunk.js"
+  },
+  {
+    "revision": "2716bd4ac1ec1323e2cb",
+    "url": "/git-desktop/static/js/runtime-main.ff51bb58.js"
+  }
+]);
